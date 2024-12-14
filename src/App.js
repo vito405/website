7815ad3f7aa05dd7f881230aebc3e9ff48486629
vito m <vitomortalo@gmail.com>
2024-12-14 13:34:56 -0700

    added to github

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/Header';
+// import About from './components/About';
+// import Skills from './components/Skills';
+// import Projects from './components/Projects';
+// import Contact from './components/Contact';
+// import Footer from './components/Footer';
+
+function App() {
+  return (
+    <Header />
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Header(){
+    return(
+    <div id="header-Contaner">
+        <a href="#Home">
+            <span>Home</span>
+        </a>
+        <a href="#About">
+            <span>About</span>
+        </a>
+        <a href="#SKills">
+            <span>Skills</span>
+        </a>
+        <a href="#Projects">
+            <span>Projects</span>
+        </a>
+        <a href="#contact">
+            <span>contact</span>
+        </a>
+    </div>
+    )
+}
